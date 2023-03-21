ce663a241fb56d328c9404fc9bcf60b79f354c25
pzdesu <pakerchang@gmail.com>
2023-03-21 20:52:12 +0800

    :sparkles: Add Drag event.

--- src/hooks/useTaskDragAndDrop.ts
+++ src/hooks/useTaskDragAndDrop.ts
@@ -27,52 +27,51 @@ export function useTaskDragAndDrop<T extends HTMLElement>(
     }),
   });
 
-  const [_, drop] = useDrop<DragItem, void, unknown>({
-    accept: ItemType.TASK,
-    hover: (item, monitor) => {
-      if (!ref.current) return;
-      if (item.from !== task.column) return;
-
-      const draggedItemIndex = item.index;
-      const hoverdItemIndex = index;
-
-      if (draggedItemIndex === hoverdItemIndex) return;
-
-      const isDraggedItemAboveHovered = draggedItemIndex < hoverdItemIndex;
-      const isDraggedItemBelowHovered = !isDraggedItemAboveHovered;
-
-      // get mouse coordinatees
-      const { x: mouseX, y: mouseY } = monitor.getClientOffset() as XYCoord;
-
-      const hoveredBoundingRect = ref.current.getBoundingClientRect();
-      // get hover item rectangle
-      const hoveredMiddleHeight =
-        (hoveredBoundingRect.bottom - hoveredBoundingRect.top) / 2;
-
-      const mouseYRelativeToHovered = mouseY - hoveredBoundingRect.top;
-      const isMouseYAboveHoveredMiddleHeight =
-        mouseYRelativeToHovered < hoveredMiddleHeight;
-      const isMouseYBelowHoveredMiddleHeight =
-        mouseYRelativeToHovered > hoveredMiddleHeight;
-
-      if (isDraggedItemAboveHovered && isMouseYAboveHoveredMiddleHeight) return;
-      if (isDraggedItemBelowHovered && isMouseYBelowHoveredMiddleHeight) return;
-
-      // Time to actually perform the action
-      handleDropHover(draggedItemIndex, hoverdItemIndex);
-
-      // Note: we're mutating the monitor item here!
-      // Generally it's better to avoid mutations,
-      // but it's good here for the sake of performance
-      // to avoid expensive index searches.
-      item.index = hoverdItemIndex;
-    },
-  });
-
-  drag(drop(ref));
+  drag(ref);
 
   return {
     ref,
     isDragging,
   };
+  // const [_, drop] = useDrop<DragItem, void, unknown>({
+  //   accept: ItemType.TASK,
+  //   hover: (item, monitor) => {
+  //     if (!ref.current) return;
+  //     if (item.from !== task.column) return;
+  //
+  //     const draggedItemIndex = item.index;
+  //     const hoverdItemIndex = index;
+  //
+  //     if (draggedItemIndex === hoverdItemIndex) return;
+  //
+  //     const isDraggedItemAboveHovered = draggedItemIndex < hoverdItemIndex;
+  //     const isDraggedItemBelowHovered = !isDraggedItemAboveHovered;
+  //
+  //     // get mouse coordinatees
+  //     const { x: mouseX, y: mouseY } = monitor.getClientOffset() as XYCoord;
+  //
+  //     const hoveredBoundingRect = ref.current.getBoundingClientRect();
+  //     // get hover item rectangle
+  //     const hoveredMiddleHeight =
+  //       (hoveredBoundingRect.bottom - hoveredBoundingRect.top) / 2;
+  //
+  //     const mouseYRelativeToHovered = mouseY - hoveredBoundingRect.top;
+  //     const isMouseYAboveHoveredMiddleHeight =
+  //       mouseYRelativeToHovered < hoveredMiddleHeight;
+  //     const isMouseYBelowHoveredMiddleHeight =
+  //       mouseYRelativeToHovered > hoveredMiddleHeight;
+  //
+  //     if (isDraggedItemAboveHovered && isMouseYAboveHoveredMiddleHeight) return;
+  //     if (isDraggedItemBelowHovered && isMouseYBelowHoveredMiddleHeight) return;
+  //
+  //     // Time to actually perform the action
+  //     handleDropHover(draggedItemIndex, hoverdItemIndex);
+  //
+  //     // Note: we're mutating the monitor item here!
+  //     // Generally it's better to avoid mutations,
+  //     // but it's good here for the sake of performance
+  //     // to avoid expensive index searches.
+  //     item.index = hoverdItemIndex;
+  //   },
+  // });
 }
