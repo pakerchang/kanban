354f6824dff283b627cb2f26ff2a2e4dd4a9aaa5
pzdesu <pakerchang@gmail.com>
2023-03-29 08:46:52 +0800

    feat(custom): restore example code explain.

--- src/components/kanban/Task.tsx
+++ src/components/kanban/Task.tsx
@@ -29,58 +29,55 @@ function Task(props: TaskProps) {
   };
 
   return (
-    <>
-      {console.log(isDragging)}
-      <Box
-        ref={ref}
-        as="div"
-        role="group"
-        position="relative"
-        rounded="lg"
-        w={200}
-        pt={3}
-        pr={3}
-        pb={1}
-        pl={4}
-        boxShadow="xl"
-        cursor="grab"
-        bgColor={task.color}
-        opacity={isDragging ? 0.5 : 1}
-      >
-        <IconButton
-          position="absolute"
-          top={0}
-          right={0}
-          zIndex={100}
-          aria-label="delete-task"
-          size="md"
-          colorScheme="solid"
-          color="gray.700"
-          icon={<DeleteIcon />}
-          opacity={0}
-          _groupHover={{
-            opacity: 1,
-          }}
-          onClick={handleDeleteClick}
-        />
-        <AutoResizeTextarea
-          value={task.title}
-          onChange={handleTitleChange}
-          fontWeight="semibold"
-          cursor="inherit"
-          p={0}
-          resize="none"
-          minH={70}
-          maxH={200}
-          border="none"
-          focusBorderColor="none"
-          color="gray.700"
-          _focus={{
-            boxShadow: "none",
-          }}
-        />
-      </Box>
-    </>
+    <Box
+      ref={ref}
+      as="div"
+      role="group"
+      position="relative"
+      rounded="lg"
+      w={200}
+      pt={3}
+      pr={3}
+      pb={1}
+      pl={4}
+      boxShadow="xl"
+      cursor="grab"
+      bgColor={task.color}
+      opacity={isDragging ? 0.5 : 1}
+    >
+      <IconButton
+        position="absolute"
+        top={0}
+        right={0}
+        zIndex={100}
+        aria-label="delete-task"
+        size="md"
+        colorScheme="solid"
+        color="gray.700"
+        icon={<DeleteIcon />}
+        opacity={0}
+        _groupHover={{
+          opacity: 1,
+        }}
+        onClick={handleDeleteClick}
+      />
+      <AutoResizeTextarea
+        value={task.title}
+        onChange={handleTitleChange}
+        fontWeight="semibold"
+        cursor="inherit"
+        p={0}
+        resize="none"
+        minH={70}
+        maxH={200}
+        border="none"
+        focusBorderColor="none"
+        color="gray.700"
+        _focus={{
+          boxShadow: "none",
+        }}
+      />
+    </Box>
   );
 }
 
